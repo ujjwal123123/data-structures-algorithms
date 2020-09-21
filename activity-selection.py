0145c1b34abd38ba92a8d010968cdf10f4e535c6
Ujjwal Goel <ujjwal234goel@gmail.com>
2020-09-21 14:06:20 +0530

    Use functions and fix a bug

--- activity-selection.py
+++ activity-selection.py
@@ -1,4 +1,6 @@
 class activity:
+    # [start, end)
+    # start is included in the interval, but end is not
     def __init__(self, start, end):
         self.start = start
         self.end = end
@@ -7,17 +9,26 @@ class activity:
         return f"({self.start}, {self.end})"
 
 
-activities_count = int(input())
-activities = []
-for _ in range(activities_count):
-    start, end = map(int, input().split())
-    activities.append(activity(start, end))
+def activity_selection(activities):
+    activities = sorted(activities,  key=lambda activity: activity.end)
 
-activities = sorted(activities,  key=lambda activity: activity.end)
+    solutions = [activities[0]]
+    for activity in activities[1:]:
+        if solutions[-1].end <= activity.start:
+            solutions.append(activity)
 
-solutions = [activities[0]]
-for activity in activities:
-    if solutions[-1].end <= activity.start:
-        solutions.append(activity)
+    return solutions
 
-print(solutions)
+
+def get_activities():
+    activities_count = int(input())
+    activities = []
+    for _ in range(activities_count):
+        start, end = map(int, input().split())
+        activities.append(activity(start, end))
+
+    return activities
+
+
+activities = get_activities()
+print(activity_selection(activities))
